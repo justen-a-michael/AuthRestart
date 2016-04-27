7b2cf7687da04e8239815037bac257d62cf666ee
Justen Michael <justen.a.michael@gmail.com>
2016-04-27 04:34:24 -0600

    password validations

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
 using SportsStore.WebUI.Infrastructure;
+using SportsStore.WebUI.Models;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -14,6 +17,38 @@ namespace SportsStore.WebUI.Controllers
         public ActionResult Index()
         {
             return View(UserManager.Users);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user,
+                    model.password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                } else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach( string error in result.Errors )
+            {
+                ModelState.AddModelError("", error);
+            }
         }
 
         private AppUserManager UserManager
---
+++ SportsStore.WebUI/Models/UserViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.WebUI.Models
+{
+    public class UserViewModels
+    {
+    }
+
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
