1992679406493f1ecabaa858e524021b26425075
Justen Michael <justen.a.michael@gmail.com>
2016-04-28 01:52:35 -0600

    uhh

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -12,28 +12,6 @@ namespace SportsStore.WebUI.Controllers {
 
     public class ProductController : Controller {
 
-        //[Authorize]
-        //public ActionResult Index()
-        //{
-        //    return View(GetData("Index"));
-        //}
-        ////[Authorize(Roles = "Users")]
-        //public ActionResult OtherAction()
-        //{
-        //    return View("Index", GetData("OtherAction"));
-        //}
-        //private Dictionary<string, object> GetData(string actionName)
-        //{
-        //    Dictionary<string, object> dict
-        //    = new Dictionary<string, object>();
-        //dict.Add("Action", actionName);
-        //    dict.Add("User", HttpContext.User.Identity.Name);
-        //    dict.Add("Authenticated", HttpContext.User.Identity.IsAuthenticated);
-        //    dict.Add("Auth Type", HttpContext.User.Identity.AuthenticationType);
-        //    dict.Add("In Users Role", HttpContext.User.IsInRole("Users"));
-        //    return dict;
-        //}
-
         private IProductRepository repository;
         public int PageSize = 4;
 
